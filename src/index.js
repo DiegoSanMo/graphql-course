64867b1f41d8a3795adfa242f4a917adc8fbd281
Diego Mosan <diego14290764@itcg.edu.mx>
2020-07-10 21:38:55 -0500

    Adding the resolvers and the typeDef

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log('sakdjaklsdjaklsd');
+import {
+    GraphQLServer
+} from 'graphql-yoga';
+//Definitions
+
+const typeDefs = `
+    type Query {
+        user: User!
+        post: Post!
+    }
+
+    type Post {
+        id: ID!,
+        title: String!
+        description: String!
+    }
+    type User {
+        name: String!
+        age: Int!
+        nationality: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        user() {
+            return {
+                name: 'Diego',
+                age: 24,
+                nationality: 'MExican'
+            }
+        },
+        post(){
+            return {
+                id: '1231',
+                title: 'First title post',
+                description: 'This is my first description'
+            }
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log('The app is running')
+});
