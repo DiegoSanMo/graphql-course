4c77e1c866dd4d4194adb24c031b73d37e2e4f60
Diego Mosan <diego14290764@itcg.edu.mx>
2020-07-14 22:39:07 -0500

    Adding the first relationship into post and users.

--- src/index.js
+++ src/index.js
@@ -3,39 +3,146 @@ import {
 } from 'graphql-yoga';
 //Definitions
 
+const users = [{
+        id: '11',
+        name: 'Diego',
+        email: "diego@hotmail.com",
+        age: 24
+    },
+    {
+        id: '12',
+        name: 'Iram',
+        email: "iram@hotmail.com",
+        age: 23
+    },
+    {
+        id: '13',
+        name: 'Joel',
+        email: "joel@hotmail.com",
+        age: 20
+    },
+    {
+        id: '14',
+        name: 'Livier',
+        email: "livier@hotmail.com",
+        age: 59
+    },
+    {
+        id: '15',
+        name: 'Balto',
+        email: "balto@hotmail.com",
+        age: 8
+    }
+];
+
+const posts = [{
+        id: "1",
+        title: "El principito",
+        body: "One of the best books",
+        published: true,
+        author: '11'
+    },
+    {
+        id: "2",
+        title: "Narnia",
+        body: "Narnia",
+        published: true,
+        author: '11'
+    },
+    {
+        id: "3",
+        title: "Cayyondi",
+        body: "The life of my dog",
+        published: false,
+        author: '13'
+    },
+    {
+        id: "4",
+        title: "The fantastic four",
+        body: "Ggg izi",
+        published: false,
+        author: '14'
+    },
+    {
+        id: "5",
+        title: "Eleanor and Park",
+        body: "A romantic book",
+        published: false,
+        author: '15'
+    }
+]
 const typeDefs = `
     type Query {
-        user: User!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         post: Post!
+        user: User!
     }
 
     type Post {
-        id: ID!,
+        id: ID!
         title: String!
-        description: String!
+        body: String!
+        published: Boolean!
+        author: User!
     }
     type User {
+        id: ID!
         name: String!
-        age: Int!
-        nationality: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
     }
 `
 
 const resolvers = {
     Query: {
+        users(parent, args, ctx, info) {
+            if (!args.query) {
+                return users;
+            }
+            return users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts;
+            }
+            const isTitleMatch = posts.filter(post => post.title.toLowerCase().includes(args.query.toLowerCase()))
+            const isBodyMatch = posts.filter(post => post.body.toLowerCase().includes(args.query.toLowerCase()))
+            return isTitleMatch || isBodyMatch;
+        },
         user() {
             return {
+                id: '123',
                 name: 'Diego',
                 age: 24,
-                nationality: 'MExican'
+                email: 'diego@hotmail.com'
             }
         },
-        post(){
+        post() {
             return {
                 id: '1231',
                 title: 'First title post',
-                description: 'This is my first description'
+                body: 'This is my first description',
+                published: false
             }
+        }
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+            console.log("parent", parent)
+            return users.find(user => user.id === parent.author)
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info){
+        console.log("posts -> parent", parent)
+        return posts.filter(post => {
+            if(post.author === parent.id){
+                console.log("posts -> post", post)
+            }
+               post.author === parent.id
+            })  
         }
     }
 }
