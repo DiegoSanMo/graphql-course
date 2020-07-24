c45efab9431c8c3ded09eb83baa6b8c74e8fe3d5
Diego Mosan <diego14290764@itcg.edu.mx>
2020-07-23 19:58:13 -0500

    The createPost mutation has been added.

--- src/index.js
+++ src/index.js
@@ -123,6 +123,7 @@ const typeDefs = `
 
     type Mutation {
         createUser(name: String!, email: String!, age: Int): User!
+        createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
     }
     type User {
         id: ID!
@@ -201,6 +202,21 @@ const resolvers = {
 
             users.push(user);
             return user;
+        },
+        createPost(parent, args, ctx, info){
+            const userExist = users.some(user => user.id === args.author)
+            if(!userExist){
+                throw new Error('User not found')
+            }
+            const post = {
+                id: uuid(),
+                title: args.title,
+                body: args.body,
+                published: args.published,
+                author: args.author
+            }
+            posts.push(post);
+            return post; 
         }
     },
     Post: {
